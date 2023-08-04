fb780bdc9529183a323d6a503c9dc85861f3df50
Arb Rahim Badsa <arbrahimbadsa@gmail.com>
2023-08-04 18:37:02 +0600

    force-dynamic configured

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,6 +3,8 @@ import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function LoginPage() {
   const supabase = createServerActionClient({ cookies });
   return (
