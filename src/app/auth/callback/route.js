62dbab2b33e5e9117d8d58ff7528fab956d5a22d
Arb Rahim Badsa <arbrahimbadsa@gmail.com>
2023-08-04 18:32:05 +0600

    force dynamic removed

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -2,8 +2,6 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
-
 export async function GET(request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
