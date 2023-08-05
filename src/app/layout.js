07025b745f91dfe084eb3ba9a5d536d31a81e1fd
Arb Rahim Badsa <arbrahimbadsa@gmail.com>
2023-08-05 15:18:15 +0600

    force dynamic

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const dynamic = "force-dynamic";
+
 export const metadata = {
   title: "Supadraw",
   description: "Draw like never before",
